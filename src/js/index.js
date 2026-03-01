523fdbc80400e006c43b65357a518441bbfa4903
unknown <joaovns0812@gmail.com>
2026-03-01 13:33:49 -0400

    Adicionado um tratamento de erros

--- src/js/index.js
+++ src/js/index.js
@@ -6,18 +6,30 @@ async function geradorDeConselhos() {
     const url = 'https://api.adviceslip.com/advice';
     const resposta = await fetch(url);
     return await resposta.json();
+    if (!resposta.ok) {
+        throw new Error('Erro ao obter o conselho');
+    }
 }
 
 async function pegarConselhos(id) {
     const url = `https://api.adviceslip.com/advice/${id}`;
     const resposta = await fetch(url);
     return await resposta.json();
+    if (!resposta.ok) {
+        throw new Error('Erro ao obter o conselho');
+    }
 }
 
 async function pegarConselhoAleatorio() {
+
+    try {
     const conselhoApi = await geradorDeConselhos();
     const conselho = await pegarConselhos(conselhoApi.slip.id);
     document.getElementById('conselho').innerText = conselho.slip.advice;
+    } catch (error) {
+        console.error('Erro ao obter conselho:', error);
+        alert('Desculpe, não foi possível obter um novo conselho no momento.');
+    }
 }
 
 pegarConselhoAleatorio()
