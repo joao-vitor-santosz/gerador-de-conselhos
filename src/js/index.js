609ae31cab361b0e9bc6cc3c65924e9b8580cb78
unknown <joaovns0812@gmail.com>
2026-02-28 23:45:39 -0400

    Site gerador de conselhos com o funcionamento de API

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+document.getElementById('btn').addEventListener('click', async function() {
+    pegarConselhoAleatorio()
+})
+
+async function geradorDeConselhos() {
+    const url = 'https://api.adviceslip.com/advice';
+    const resposta = await fetch(url);
+    return await resposta.json();
+}
+
+async function pegarConselhos(id) {
+    const url = `https://api.adviceslip.com/advice/${id}`;
+    const resposta = await fetch(url);
+    return await resposta.json();
+}
+
+async function pegarConselhoAleatorio() {
+    const conselhoApi = await geradorDeConselhos();
+    const conselho = await pegarConselhos(conselhoApi.slip.id);
+    document.getElementById('conselho').innerText = conselho.slip.advice;
+    console.log(conselho)
+}
+
+pegarConselhoAleatorio()
